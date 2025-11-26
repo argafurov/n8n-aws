704b0609fa97d5fe4b85eff723cb139c20363b14
Azamat Gafurov <azamat.gafurov@quantori.com>
2025-11-26 11:15:23 +0500

    Add Open WebUI EC2

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,3 +12,18 @@ output "n8n_instance_id" {
   value       = aws_instance.n8n.id
   description = "Instance ID for SSM Session Manager"
 }
+
+output "openwebui_public_ip" {
+  value       = aws_eip.openwebui.public_ip
+  description = "Public IP of the Open WebUI EC2 instance"
+}
+
+output "openwebui_url" {
+  value       = "https://${local.openwebui_fqdn}"
+  description = "HTTPS URL for Open WebUI"
+}
+
+output "openwebui_instance_id" {
+  value       = aws_instance.openwebui.id
+  description = "Open WebUI Instance ID for SSM Session Manager"
+}
